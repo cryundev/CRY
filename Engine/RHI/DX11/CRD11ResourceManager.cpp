c84d69526bd202edab5dd7f791154182278037c8
yunei <yunei0313@gmail.com>
2024-12-21 21:35:10 +0900

    ResourceManager 매크로 코드로 변경

--- Engine/RHI/DX11/CRD11ResourceManager.cpp
+++ Engine/RHI/DX11/CRD11ResourceManager.cpp
@@ -3,6 +3,7 @@
 #include "CRD11IndexBuffer.h"
 #include "CRD11InputLayout.h"
 #include "CRD11PixelShader.h"
+#include "CRD11RenderTargetView.h"
 #include "CRD11SamplerState.h"
 #include "CRD11ShaderResourceView.h"
 #include "CRD11Texture2D.h"
@@ -10,59 +11,42 @@
 #include "CRD11VertexShader.h"
 
 
+#define CLEAR_CRD11_RESOURCE_MANAGE_TYPE( ResourceType ) \
+    CRD11##ResourceType##s.clear(); \
+
+
+#define GET_CRD11_RESOURCE_MANAGE_TYPE( ResourceType ) \
+    CRD11##ResourceType##SPtr CRD11ResourceManager::Get##ResourceType( const CRName& Name ) \
+    { \
+        return _GetResource< CRD11##ResourceType >( Name, CRD11##ResourceType##s ); \
+    } \
+
+
 //---------------------------------------------------------------------------------------------------------------------
 /// Clear
 //---------------------------------------------------------------------------------------------------------------------
 void CRD11ResourceManager::Clear()
 {
-	VertexBuffers.clear();
-	IndexBuffers .clear();
-	InputLayouts .clear();
-	VertexShaders.clear();
-	PixelShaders .clear();
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( VertexBuffer       )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( IndexBuffer        )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( InputLayout        )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( CompiledShader     )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( VertexShader       )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( PixelShader        )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( Texture2D          )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( ShaderResourceView )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( SamplerState       )
+    CLEAR_CRD11_RESOURCE_MANAGE_TYPE( RenderTargetView   )
 }
 
-CRD11VertexBufferSPtr CRD11ResourceManager::GetVertexBuffer( const CRName& Name )
-{
-	return _GetResource< CRD11VertexBuffer >( Name, VertexBuffers );
-}
 
-CRD11IndexBufferSPtr CRD11ResourceManager::GetIndexBuffer( const CRName& Name )
-{
-	return _GetResource< CRD11IndexBuffer >( Name, IndexBuffers );
-}
-
-CRD11InputLayoutSPtr CRD11ResourceManager::GetInputLayout( const CRName& Name )
-{
-	return _GetResource< CRD11InputLayout >( Name, InputLayouts );
-}
-
-CRD11CompiledShaderSPtr CRD11ResourceManager::GetCompiledShader( const CRName& Name )
-{
-	return _GetResource< CRD11CompiledShader >( Name, CompiledShaders );
-}
-
-CRD11VertexShaderSPtr CRD11ResourceManager::GetVertexShader( const CRName& Name )
-{
-	return _GetResource< CRD11VertexShader >( Name, VertexShaders );
-}
-
-CRD11PixelShaderSPtr CRD11ResourceManager::GetPixelShader( const CRName& Name )
-{
-	return _GetResource< CRD11PixelShader >( Name, PixelShaders );
-}
-
-CRD11Texture2DSPtr CRD11ResourceManager::GetTexture2D( const CRName& Name )
-{
-	return _GetResource< CRD11Texture2D >( Name, Texture2Ds );
-}
-
-CRD11ShaderResourceViewSPtr CRD11ResourceManager::GetShaderResourceView( const CRName& Name )
-{
-	return _GetResource< CRD11ShaderResourceView >( Name, ShaderResourceViews );
-}
-
-CRD11SamplerStateSPtr CRD11ResourceManager::GetSamplerState( const CRName& Name )
-{
-	return _GetResource< CRD11SamplerState >( Name, SamplerStates );
-}
+GET_CRD11_RESOURCE_MANAGE_TYPE( VertexBuffer       )
+GET_CRD11_RESOURCE_MANAGE_TYPE( IndexBuffer        )
+GET_CRD11_RESOURCE_MANAGE_TYPE( InputLayout        )
+GET_CRD11_RESOURCE_MANAGE_TYPE( CompiledShader     )
+GET_CRD11_RESOURCE_MANAGE_TYPE( VertexShader       )
+GET_CRD11_RESOURCE_MANAGE_TYPE( PixelShader        )
+GET_CRD11_RESOURCE_MANAGE_TYPE( Texture2D          )
+GET_CRD11_RESOURCE_MANAGE_TYPE( ShaderResourceView )
+GET_CRD11_RESOURCE_MANAGE_TYPE( SamplerState       )
+GET_CRD11_RESOURCE_MANAGE_TYPE( RenderTargetView   )
--- Engine/RHI/DX11/CRD11ResourceManager.h
+++ Engine/RHI/DX11/CRD11ResourceManager.h
@@ -6,32 +6,27 @@
 #include "../../Core/Containers/CRContainerInc.h"
 
 
+#define DECLARE_CRD11_RESOURCE_MANAGE_TYPE( ResourceType ) \
+    public: using CRD11##ResourceType##Map = CRMap< CRName, CRD11##ResourceType##SPtr >; \
+    private: CRD11##ResourceType##Map CRD11##ResourceType##s; \
+    public: CRD11##ResourceType##SPtr Get##ResourceType( const CRName& Name ); \
+
+
 //---------------------------------------------------------------------------------------------------------------------
 /// CRD11ResourceManager
 //---------------------------------------------------------------------------------------------------------------------
 class CRD11ResourceManager
 {
-public:
-	using CRD11VertexBufferMap       = CRMap< CRName, CRD11VertexBufferSPtr       >;
-	using CRD11IndexBufferMap        = CRMap< CRName, CRD11IndexBufferSPtr        >;
-	using CRD11InputLayoutMap        = CRMap< CRName, CRD11InputLayoutSPtr        >;
-	using CRD11compiledShaderMap     = CRMap< CRName, CRD11CompiledShaderSPtr     >;
-	using CRD11PixelShaderMap        = CRMap< CRName, CRD11PixelShaderSPtr        >;
-	using CRD11VertexShaderMap       = CRMap< CRName, CRD11VertexShaderSPtr       >;
-	using CRD11Texture2DMap          = CRMap< CRName, CRD11Texture2DSPtr          >;
-	using CRD11ShaderResourceViewMap = CRMap< CRName, CRD11ShaderResourceViewSPtr >;
-	using CRD11SamplerStateMap       = CRMap< CRName, CRD11SamplerStateSPtr       >;
-
-private:
-	CRD11VertexBufferMap       VertexBuffers;
-	CRD11IndexBufferMap        IndexBuffers;
-	CRD11InputLayoutMap        InputLayouts;
-	CRD11compiledShaderMap     CompiledShaders;
-	CRD11VertexShaderMap       VertexShaders;
-	CRD11PixelShaderMap        PixelShaders;
-	CRD11Texture2DMap	       Texture2Ds;
-	CRD11ShaderResourceViewMap ShaderResourceViews;
-	CRD11SamplerStateMap       SamplerStates;
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( VertexBuffer       )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( IndexBuffer        )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( InputLayout        )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( CompiledShader     )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( VertexShader       )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( PixelShader        )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( Texture2D          )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( SamplerState       )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( ShaderResourceView )
+    DECLARE_CRD11_RESOURCE_MANAGE_TYPE( RenderTargetView   )
 	
 public:
 	/// Constructor
@@ -39,33 +34,6 @@ public:
 
 	/// Clear
 	void Clear();
-	
-	/// Add vertex buffer.
-	CRD11VertexBufferSPtr GetVertexBuffer( const CRName& Name );
-
-	/// Add index buffer.
-	CRD11IndexBufferSPtr GetIndexBuffer( const CRName& Name );
-
-	/// Add input layout.
-	CRD11InputLayoutSPtr GetInputLayout( const CRName& Name );
-
-	/// Add compiled shader.
-	CRD11CompiledShaderSPtr GetCompiledShader( const CRName& Name );
-
-	/// Add vertex shader.
-	CRD11VertexShaderSPtr GetVertexShader( const CRName& Name );
-
-	/// Add pixel shader.
-	CRD11PixelShaderSPtr GetPixelShader( const CRName& Name );
-
-	/// Add texture2D.
-	CRD11Texture2DSPtr GetTexture2D( const CRName& Name );
-
-	/// Add shader resource view.
-	CRD11ShaderResourceViewSPtr GetShaderResourceView( const CRName& Name );
-
-	/// Add sampler state.
-	CRD11SamplerStateSPtr GetSamplerState( const CRName& Name );
 
 private:
 	/// Get resource.
