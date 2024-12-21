7557f343a6d904749653bab923923818066572d1
yunei <yunei0313@gmail.com>
2024-12-21 22:01:58 +0900

    코드 정리

--- Engine/RHI/DX11/CRD11IndexBuffer.h
+++ Engine/RHI/DX11/CRD11IndexBuffer.h
@@ -13,7 +13,8 @@ class CRD11IndexBuffer : public CRD11Object< ID3D11Buffer >
 private:
     unsigned int Count = 0;
 
-public:/// Create index buffer.
+public:
+    /// Create index buffer.
     void Create( D3D11_USAGE Usage, unsigned int CpuAccess, const CRArray< int >& Indice );
 
     /// Get count.
