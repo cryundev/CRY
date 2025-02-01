8ad6a2efd31b54bfa062d560b9867f9d9e7e9bab
yunei <yunei0313@gmail.com>
2025-02-01 22:11:35 +0900

    [Editor_WPF] 추가

---
+++ Editor_WPF/Common/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+
+namespace Editor_WPF
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged( string propertyName )
+        {
+            PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+    }
+}
---
+++ Editor_WPF/GameProject/OpenProjectView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+
+namespace Editor_WPF.GameProject;
+
+
+public partial class OpenProjectView : UserControl
+{
+    public OpenProjectView()
+    {
+        InitializeComponent();
+    }
+}
