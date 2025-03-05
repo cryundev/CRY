6edf452dea7a2cc3ce2fd7b9b54fee85d566c7cc
yunei0313 <yunei0313@gmail.com>
2025-03-05 15:26:37 +0900

    [WPF][GEP008] - 로그 메시지 컬러링 - 로그 메시지 비우기 및 필터 버튼을 통해 필터링

--- Editor_WPF/Utilities/LoggerView.xaml.cs
+++ Editor_WPF/Utilities/LoggerView.xaml.cs
@@ -1,11 +1,42 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+
 
 namespace Editor_WPF.Utilities;
 
+
+//---------------------------------------------------------------------------------------------------------------------
+/// LoggerView
+//---------------------------------------------------------------------------------------------------------------------
 public partial class LoggerView : UserControl
 {
+    //-----------------------------------------------------------------------------------------------------------------
+    /// LoggerView
+    //-----------------------------------------------------------------------------------------------------------------
     public LoggerView()
     {
         InitializeComponent();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// OnClearButtonClicked
+    //-----------------------------------------------------------------------------------------------------------------
+    private void OnClearButtonClicked( object sender, RoutedEventArgs e )
+    {
+        Logger.Clear();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// OnMessageFilterButtonClicked
+    //-----------------------------------------------------------------------------------------------------------------
+    private void OnMessageFilterButtonClicked( object sender, RoutedEventArgs e )
+    {
+        int filter = 0x0;
+        
+        if ( toggleInfo.IsChecked    == true ) filter |= (int)MessageType.Info;
+        if ( toggleWarning.IsChecked == true ) filter |= (int)MessageType.Warning;
+        if ( toggleError.IsChecked   == true ) filter |= (int)MessageType.Error;
+
+        Logger.SetMessageFilter( filter );
     }
 }
