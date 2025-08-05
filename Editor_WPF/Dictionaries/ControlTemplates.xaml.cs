971c3b84c893341e3d9936b4a4a8a22aa2ddf549
CRY <yunei0313@gmail.com>
2025-08-05 11:23:50 +0900

    [Editor][WPF] - 누락된 파일 서밋

---
+++ Editor_WPF/Dictionaries/ControlTemplates.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+
+namespace Editor_WPF.Dictionaries;
+
+
+//---------------------------------------------------------------------------------------------------------------------
+/// ControlTemplates
+//---------------------------------------------------------------------------------------------------------------------
+public partial class ControlTemplates : ResourceDictionary
+{
+    //-----------------------------------------------------------------------------------------------------------------
+    /// OnTextBoxKeyDowned
+    //-----------------------------------------------------------------------------------------------------------------
+    private void OnTextBoxKeyDowned( object sender, System.Windows.Input.KeyEventArgs e )
+    {
+        TextBox? textBox = sender as TextBox;
+        BindingExpression? exp = textBox?.GetBindingExpression( TextBox.TextProperty );
+        if ( exp == null ) return;
+
+        if ( e.Key == Key.Enter )
+        {
+            if ( textBox?.Tag is ICommand command && command.CanExecute( textBox.Text ) )
+            {
+                command.Execute( textBox.Text );
+            }
+            else
+            {
+                exp.UpdateSource();
+            }
+
+            Keyboard.ClearFocus();
+            e.Handled = true;
+        }
+        else if ( e.Key == Key.Escape )
+        {
+            exp.UpdateTarget();
+            Keyboard.ClearFocus();
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// OnRenameTextBoxKeyDowned
+    //-----------------------------------------------------------------------------------------------------------------
+    private void OnRenameTextBoxKeyDowned( object sender, KeyEventArgs e )
+    {
+        TextBox? textBox = sender as TextBox;
+        BindingExpression? exp = textBox?.GetBindingExpression( TextBox.TextProperty );
+        if ( exp == null ) return;
+
+        if ( e.Key == Key.Enter )
+        {
+            if ( textBox?.Tag is ICommand command && command.CanExecute( textBox.Text ) )
+            {
+                command.Execute( textBox.Text );
+            }
+            else
+            {
+                exp.UpdateSource();
+            }
+
+            if ( textBox != null )
+            {
+                textBox.Visibility = Visibility.Collapsed;
+            }
+
+            e.Handled = true;
+        }
+        else if ( e.Key == Key.Escape )
+        {
+            exp.UpdateTarget();
+            
+            if ( textBox != null )
+            {
+                textBox.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    /// OnRenameTextBoxLostFocus
+    //-----------------------------------------------------------------------------------------------------------------
+    private void OnRenameTextBoxLostFocus( object sender, RoutedEventArgs e )
+    {
+        TextBox? textBox = sender as TextBox;
+        if ( textBox is { IsVisible: false } ) return;
+        
+        BindingExpression? exp = textBox?.GetBindingExpression( TextBox.TextProperty );
+        if ( exp == null ) return;
+
+        exp.UpdateTarget();
+        
+        if ( textBox != null )
+        {
+            textBox.Visibility = Visibility.Collapsed;
+        }
+    }
+}
