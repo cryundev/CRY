f8d46244d9352a856049873a3a595105d434d053
CRY <yunei0313@gmail.com>
2026-02-14 14:25:40 +0900

    - 렌더 타겟 2D 추가

---
+++ Engine/Source/RHI/DX11/CRD11RenderTarget2D.cpp
@@ -0,0 +1,111 @@
+﻿#include "CRD11RenderTarget2D.h"
+#include "CRD11.h"
+#include "CRD11ResourceManager.h"
+#include "Resource/CRD11RenderTargetView.h"
+#include "Resource/CRD11ShaderResourceView.h"
+#include "Resource/CRD11Texture2D.h"
+
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Create render target.
+//---------------------------------------------------------------------------------------------------------------------
+void CRD11RenderTarget2D::Create( const CRName& BaseName, u32 InWidth, u32 InHeight, DXGI_FORMAT InFormat )
+{
+    Width  = InWidth;
+    Height = InHeight;
+    Format = InFormat;
+
+    const CRName textureName = BaseName + "_Texture2D";
+    const CRName rtvName     = BaseName + "_RTV";
+    const CRName srvName     = BaseName + "_SRV";
+
+    Texture = GD11RM.GetTexture2D( textureName );
+    if ( !Texture.expired() )
+    {
+        Texture.lock()->SetObjectPtr( nullptr );
+
+        D3D11_TEXTURE2D_DESC td;
+        ZeroMemory( &td, sizeof( D3D11_TEXTURE2D_DESC ) );
+
+        td.Width            = Width;
+        td.Height           = Height;
+        td.MipLevels        = 1;
+        td.ArraySize        = 1;
+        td.Format           = Format;
+        td.SampleDesc.Count = 1;
+        td.Usage            = D3D11_USAGE_DEFAULT;
+        td.BindFlags        = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+
+        Texture.lock()->Create( td );
+    }
+
+    RenderTargetView = GD11RM.GetRenderTargetView( rtvName );
+    if ( !RenderTargetView.expired() )
+    {
+        RenderTargetView.lock()->SetObjectPtr( nullptr );
+        RenderTargetView.lock()->Create( Texture.lock()->GetObjectPtr(), nullptr );
+    }
+
+    ShaderResourceView = GD11RM.GetShaderResourceView( srvName );
+    if ( !ShaderResourceView.expired() )
+    {
+        ShaderResourceView.lock()->SetObjectPtr( nullptr );
+
+        D3D11_SHADER_RESOURCE_VIEW_DESC sd;
+        ZeroMemory( &sd, sizeof( D3D11_SHADER_RESOURCE_VIEW_DESC ) );
+
+        sd.Format                    = Format;
+        sd.ViewDimension             = D3D11_SRV_DIMENSION_TEXTURE2D;
+        sd.Texture2D.MipLevels       = 1;
+        sd.Texture2D.MostDetailedMip = 0;
+
+        ShaderResourceView.lock()->Create( Texture.lock()->GetObjectPtr(), sd );
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Release resources.
+//---------------------------------------------------------------------------------------------------------------------
+void CRD11RenderTarget2D::Release()
+{
+    if ( !ShaderResourceView.expired() ) ShaderResourceView.lock()->SetObjectPtr( nullptr );
+    if ( !RenderTargetView.expired() )  RenderTargetView.lock()->SetObjectPtr( nullptr );
+    if ( !Texture.expired() )           Texture.lock()->SetObjectPtr( nullptr );
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Resize.
+//---------------------------------------------------------------------------------------------------------------------
+void CRD11RenderTarget2D::Resize( const CRName& BaseName, u32 InWidth, u32 InHeight, DXGI_FORMAT InFormat )
+{
+    Release();
+    Create( BaseName, InWidth, InHeight, InFormat );
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Get render target view.
+//---------------------------------------------------------------------------------------------------------------------
+ID3D11RenderTargetView* CRD11RenderTarget2D::GetRTV() const
+{
+    if ( RenderTargetView.expired() ) return nullptr;
+
+    return RenderTargetView.lock()->GetObjectPtr();
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Get shader resource view.
+//---------------------------------------------------------------------------------------------------------------------
+ID3D11ShaderResourceView* CRD11RenderTarget2D::GetSRV() const
+{
+    if ( ShaderResourceView.expired() ) return nullptr;
+
+    return ShaderResourceView.lock()->GetObjectPtr();
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/// Is valid.
+//---------------------------------------------------------------------------------------------------------------------
+bool CRD11RenderTarget2D::IsValid() const
+{
+    return GetRTV() && GetSRV();
+}
---
+++ Engine/Source/RHI/DX11/CRD11RenderTarget2D.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+
+#include "CRD11Types.h"
+#include "Source/Core/CRTypes.h"
+#include "Source/Core/Strings/CRStringInc.h"
+
+
+#include <d3d11.h>
+
+
+//---------------------------------------------------------------------------------------------------------------------
+/// CRD11RenderTarget2D
+//---------------------------------------------------------------------------------------------------------------------
+class CRD11RenderTarget2D
+{
+private:
+    CRD11Texture2DWPtr          Texture;
+    CRD11RenderTargetViewWPtr   RenderTargetView;
+    CRD11ShaderResourceViewWPtr ShaderResourceView;
+
+    u32         Width  = 0;
+    u32         Height = 0;
+    DXGI_FORMAT Format = DXGI_FORMAT_UNKNOWN;
+
+public:
+    /// Create render target.
+    void Create( const CRName& BaseName, u32 InWidth, u32 InHeight, DXGI_FORMAT InFormat );
+
+    /// Release resources.
+    void Release();
+
+    /// Resize.
+    void Resize( const CRName& BaseName, u32 InWidth, u32 InHeight, DXGI_FORMAT InFormat );
+
+    /// Get render target view.
+    ID3D11RenderTargetView* GetRTV() const;
+
+    /// Get shader resource view.
+    ID3D11ShaderResourceView* GetSRV() const;
+
+    /// Is valid.
+    bool IsValid() const;
+};
